7e04662533e244a2be47ccab2b6830df0fa43699
unknown <subhash227@yahoo.com>
2021-04-24 02:56:15 +1000

    index and dot env updated

--- index.js
+++ index.js
@@ -15,43 +15,6 @@ var db = mysql.createPool({
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_DATABASE_NAME || "devtest",
 });
-// var dbConfig = {
-//   host: process.env.DB_HOST || "sql156.main-hosting.eu",
-//   user: process.env.DB_USER || "u914229342_subhash227",
-//   password: process.env.DB_PASSWORD || "Mysqldatabase@999",
-//   database: process.env.DB_DATABASE_NAME || "u914229342_devtest",
-// };
-
-// var connection;
-// function handleDisconnect() {
-//   connection = mysql.createConnection(dbConfig); // Recreate the connection, since the old one cannot be reused.
-//   connection.connect(function onConnect(err) {
-//     // The server is either down
-//     if (err) {
-//       // or restarting (takes a while sometimes).
-//       console.log("error when connecting to db:", err);
-//       console.log("connected");
-//       setTimeout(handleDisconnect, 10000); // We introduce a delay before attempting to reconnect,
-//     } // to avoid a hot loop, and to allow our node script to
-//   }); // process asynchronous requests in the meantime.
-//   // If you're also serving http, display a 503 error.
-//   connection.on("error", function onError(err) {
-//     console.log("db error", err);
-//     if (err.code == "PROTOCOL_CONNECTION_LOST" || err.code == "ECONNRESET") {
-//       // Connection to the MySQL server is usually
-//       handleDisconnect(); // lost due to either server restart, or a
-//     } else {
-//       // connnection idle timeout (the wait_timeout
-//       throw err; // server variable configures this)
-//     }
-//   });
-// }
-// handleDisconnect();
-// db.connect((err) => {
-//   if (err) throw err;
-//   console.log("My SQL connected...");
-// });
-
 app.get("/", (req, res) => {
   res.render("index", { message: "" });
 });
